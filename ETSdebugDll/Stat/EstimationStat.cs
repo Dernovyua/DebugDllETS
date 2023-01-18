0de6e9eb7dfbe488699efa747918f09d2ea6c956
ChernyavskiyV <108263860+ChernyavskiyV@users.noreply.github.com>
2023-01-18 16:55:47 +0300

    16-55

--- ETSdebugDll/Stat/EstimationStat.cs
+++ ETSdebugDll/Stat/EstimationStat.cs
@@ -15,14 +15,16 @@ using System.Data;
 using System.Windows.Media;
 using System.Windows.Documents;
 using System.Diagnostics.SymbolStore;
+using System.Windows.Controls;
+using ScriptSolution.Model;
 
 namespace EstimationStat
 {
     public class UserParam
     {
-        string  name = "";
-        double  value = 0;
-        int     digits = 6;
+        public string  name = "";
+        public double value = 0;
+        public int digits = 6;
     }
     public class EstimationStat
     {
@@ -31,7 +33,7 @@ namespace EstimationStat
         public double recoveryFactor = 0; // Факттор восстановления
         public double averageDeal = 0; // Средняя прибыль на сделку
         public double dealCount = 0; // Количество сделок 
-        List<UserParam> userParam;
+        public List<UserParam> userParam;// Параметры оптимизации
     };
 
     public class StatisticDebugScript : StatisticScript
@@ -44,80 +46,112 @@ namespace EstimationStat
         {
             if (_passCount == 0)
                 _estStat = new List<EstimationStat>();
-            
-            if( stat.FactorRecovery >= 2 )
+
+            var oParams = OptimParams;
+            var uStat = UserStatistics;
+
+            if( stat.FactorRecovery >= uStat[0].Value )
             {
                 EstimationStat est = new EstimationStat();
+                est.userParam = new List<UserParam>();
+
                 est.profit = stat.NetProfitLoss;
                 est.averageDeal = stat.AvaregeProfit;
                 est.recoveryFactor = stat.FactorRecovery;
                 est.dealCount = stat.TradeDealsList.Count;
                 est.drawDown = stat.MaxDrownDown;
+                
+                for(int i=0; i< oParams.Count; i++)
+                {
+                    UserParam up = new UserParam();
+                    up.name = oParams[i].Name;
+                    up.value = oParams[i].Value;
+                    est.userParam.Add(up);
+                }
                 _estStat.Add(est);
             }
 
             _passCount++;
 
-            if (_passCount >= 5)
+            if (_passCount == 10)
+            {
                 SaveResultCSV("C:\\DOCS\\out.csv", ";");
+                //SaveResultPDF("C:\\DOCS\\out.pdf");
+            }
         }
 
+        #region Сохранение результатов в PDF 
+        
         void SaveResultPDF( string fNameFull )
         {
-            iTextSharp.text.Document doc = new iTextSharp.text.Document();
-            PdfWriter.GetInstance(doc, new FileStream( fNameFull, FileMode.Create));
-            doc.Open();
-            //BaseFont baseFont = BaseFont.CreateFont("C:\\Windows\\Fonts\\arial.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
-            //iTextSharp.text.Font font = new iTextSharp.text.Font(baseFont, iTextSharp.text.Font.DEFAULTSIZE, iTextSharp.text.Font.NORMAL);
-            PdfPTable table = new PdfPTable(5);
-
-            // Добавляем заголовки 
-            PdfPCell cell = new PdfPCell(new Phrase(new Phrase("Profit")));
-            cell.BackgroundColor = iTextSharp.text.BaseColor.YELLOW;
-            table.AddCell(cell);
-
-            cell = new PdfPCell(new Phrase(new Phrase("Max DD")));
-            cell.BackgroundColor = iTextSharp.text.BaseColor.YELLOW;
-            table.AddCell(cell);
-
-            cell = new PdfPCell(new Phrase(new Phrase("Recovery")));
-            cell.BackgroundColor = iTextSharp.text.BaseColor.YELLOW;
-            table.AddCell(cell);
-
-            cell = new PdfPCell(new Phrase(new Phrase("Avg. Deal")));
-            cell.BackgroundColor = iTextSharp.text.BaseColor.YELLOW;
-            table.AddCell(cell);
-
-            cell = new PdfPCell(new Phrase(new Phrase("Deal Count")));
-            cell.BackgroundColor = iTextSharp.text.BaseColor.YELLOW;
-            table.AddCell(cell);
-            
-            for ( int i=0; i<_estStat.Count; i++ )
+            var document = new iTextSharp.text.Document();
+            using (var writer = PdfWriter.GetInstance(document, new FileStream( fNameFull, FileMode.Create)))
             {
-                table.AddCell(new Phrase(_estStat[i].profit.ToString()));
-                table.AddCell(new Phrase(_estStat[i].drawDown.ToString()));
-                table.AddCell(new Phrase(_estStat[i].recoveryFactor.ToString()));
-                table.AddCell(new Phrase(_estStat[i].averageDeal.ToString()));
-                table.AddCell(new Phrase(_estStat[i].dealCount.ToString()));
-            }
+                document.Open();
+                PdfPTable table = new PdfPTable(5);
+                // Добавляем заголовки 
+                PdfPCell cell = new PdfPCell(new Phrase(new Phrase("Profit")));
+                cell.BackgroundColor = iTextSharp.text.BaseColor.YELLOW;
+                table.AddCell(cell);
 
-            doc.Add(table);
-            doc.Close();
+                cell = new PdfPCell(new Phrase(new Phrase("Max DD")));
+                cell.BackgroundColor = iTextSharp.text.BaseColor.YELLOW;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(new Phrase("Recovery")));
+                cell.BackgroundColor = iTextSharp.text.BaseColor.YELLOW;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(new Phrase("Avg. Deal")));
+                cell.BackgroundColor = iTextSharp.text.BaseColor.YELLOW;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(new Phrase("Deal Count")));
+                cell.BackgroundColor = iTextSharp.text.BaseColor.YELLOW;
+                table.AddCell(cell);
+
+                for (int i = 0; i < _estStat.Count; i++)
+                {
+                    table.AddCell(new Phrase(_estStat[i].profit.ToString()));
+                    table.AddCell(new Phrase(_estStat[i].drawDown.ToString()));
+                    table.AddCell(new Phrase(_estStat[i].recoveryFactor.ToString()));
+                    table.AddCell(new Phrase(_estStat[i].averageDeal.ToString()));
+                    table.AddCell(new Phrase(_estStat[i].dealCount.ToString()));
+                }
+
+                document.Add(table);
+
+                document.Close();
+                writer.Close();
+            }
         }
+
+        #endregion
+
+        #region Сохранение результатов в CSV
 
         void SaveResultCSV(string fNameFull, string delimiter )
         {
+            if (_estStat.Count <= 0)
+                return;
+
             using (StreamWriter sw = new StreamWriter(fNameFull, false, System.Text.Encoding.Default))
             {
                 string str = "";
 
                 for( int i = 0; i < _statNames.Length; i++ )
                 {
-                    str += _statNames[i];
+                    str += _statNames[i] + delimiter;
+                }
 
-                    if( i < _statNames.Length - 1)
+                for( int i=0; i<_estStat[0].userParam.Count; i++ )
+                {
+                    str += _estStat[0].userParam[i].name;
+
+                    if (i < _estStat[0].userParam.Count - 1)
                         str += delimiter;
                 }
+
                 sw.WriteLine(str);
 
                 for(int i=0; i<_estStat.Count; i++)
@@ -126,11 +160,27 @@ namespace EstimationStat
                     str += _estStat[i].drawDown.ToString() + delimiter;
                     str += _estStat[i].recoveryFactor.ToString() + delimiter;
                     str += _estStat[i].averageDeal.ToString() + delimiter;
-                    str += _estStat[i].dealCount.ToString();
+                    str += _estStat[i].dealCount.ToString() + delimiter; 
+
+                    for( int j=0; j< _estStat[i].userParam.Count; j++)
+                    {
+                        str += _estStat[i].userParam[j].value.ToString();
+
+                        if ( i < _estStat[i].userParam.Count - 1)
+                            str += delimiter;
+                    }
                     sw.WriteLine(str);
                 }
                 sw.Close();
             }
+        }
+
+        #endregion
+        public override List<UserParamModel> CreateUserStatisticParam()
+        {
+            List<UserParamModel> list = new List<UserParamModel>();
+            list.Add(new UserParamModel { Name = "Recovery >= ", IsOptimization = false });
+            return list;
         }
 
         public override void GetAttributes()
