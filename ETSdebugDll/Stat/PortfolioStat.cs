e21d360c4ed51b45b010707121fe4ca7a8fee361
ChernyavskiyV <108263860+ChernyavskiyV@users.noreply.github.com>
2023-03-20 15:16:57 +0300

    15-00

---
+++ ETSdebugDll/Stat/PortfolioStat.cs
@@ -0,0 +1,37 @@
+﻿using ScriptSolution.Model.Statistics;
+using ScriptSolution;
+using SourceEts.CommonSettings.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Data;
+using ScriptSolution.Model.OptimizationResul;
+using Export;
+using Export.ModelsExport;
+using Export.Models;
+using Export.Models.Charts;
+using System.Windows;
+using System.Xml.Linq;
+using System.Net;
+using Export.Enums;
+using Export.DrawingCharts;
+using System.Drawing;
+using static Export.DrawingCharts.LineETS;
+using Point = Export.DrawingCharts.LineETS.Point;
+using System.Runtime.CompilerServices;
+using System.Windows.Media.Animation;
+using ScriptSolution.Model.Portfels.PortfelTest;
+
+namespace PortfolioStat
+{
+    internal class PortfolioStat : PortfelScript
+    {     
+
+        public override List<List<int>> ExecuteEndOptimization( List< PortfelResultTest> prt, List<CorelationModel> cmdl )
+        {
+            List<List<int>> res = new List<List<int>>();
+            return res;
+        }
+    }
+}
